00d28446997eb11946b969b9bb6ecb07dbba97aa
DevelSSN <shashankshantharamnayak@gmail.com>
2026-05-14 08:24:37 +0530

    Decompose

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,8 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "plugins.lazyvim_overrides" },
+    { import = "plugins.custom" },
     { import = "plugins" },
   },
   defaults = {
