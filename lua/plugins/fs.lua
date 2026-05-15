04e387b56c6159ca4dd50b105c034cb891e18b39
DevelSSN <shashankshantharamnayak@gmail.com>
2026-05-15 12:02:41 +0530

    Fugitive

--- lua/plugins/fs.lua
+++ lua/plugins/fs.lua
@@ -3,18 +3,26 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      win_options = {
+        signcolumn = "yes:2",
+      },
+    },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
+  },
+  {
+    "refractalize/oil-git-status.nvim",
+
+    config = true,
   },
   {
     "nvim-mini/mini.icons",
     opts = {},
   },
-  { "nvim-mini/mini.comment", version = false },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
   { "nvim-mini/mini.move", version = false },
   { "nvim-mini/mini.surround", version = false },
-  { "nvim-tree/nvim-web-devicons", opts = {} },
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -22,7 +30,7 @@ return {
   },
   {
     "romus204/tree-sitter-manager.nvim",
-    dependencies = {}, -- tree-sitter CLI must be installed system-wide
+    dependencies = {},
     config = function()
       require("tree-sitter-manager").setup({})
     end,
@@ -34,5 +42,11 @@ return {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
+  },
+  {
+    "NStefan002/screenkey.nvim",
+    lazy = false,
+    version = "*",
+    opts = {},
   },
 }
