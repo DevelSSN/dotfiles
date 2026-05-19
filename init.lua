05bfd7cebb1b4a22fd4acf84b186860cb6424866
DevelSSN <shashankshantharamnayak@gmail.com>
2026-05-19 11:23:22 +0530

    Move git

--- init.lua
+++
@@ -1,7 +0,0 @@
-require("config.lazy")
-require("config.keymaps")
-require("config.opts")
-require("config.setup")
-require("config.autocmd")
-
--- NVIM_APPNAME=$XDG_CONFIG_HOME subdir custom at startup env var
--- lua/config/setup.lua
+++
@@ -1,5 +0,0 @@
-vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
-
-local lsp = vim.lsp
-
-require("config.lsp.luals")
